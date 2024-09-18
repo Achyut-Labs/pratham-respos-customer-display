963a8c7a1c2495c451d04560488ce0f0ecb37c29
Devyani-AL <devyani.patil@achyutlabs.com>
2024-09-18 14:38:45 +0530

    customer name corrections

--- src/components/CustmerScreen.vue
+++ src/components/CustmerScreen.vue
@@ -2,7 +2,7 @@
   <div class="col-6">
     <div class="text-h5 text-bold text-center q-px-sm q-pb-sm q-pt-none">
       <div class="q-pa-sm bg-primary text-white rounded-borders">
-        {{ customer?.name || 'Guest' }} {{ cartItems && cartItems.daily_order_number ? '- #' + cartItems.daily_order_number: '' }}
+        {{ cartItems?.customer?.name || cartItems?.guestCustomerName || 'Guest' }} {{ cartItems && cartItems.daily_order_number ? '- #' + cartItems.daily_order_number: '' }}
       </div>
     </div>
     <div class="">
@@ -61,12 +61,12 @@
 <script setup lang="ts">
 import { ref, computed, onMounted } from 'vue';
 import { onCartUpdate } from '../boot/local-socket';
-import { Customer, OrderCart } from '../types/cart';
+import { OrderCart } from '../types/cart';
 import CardTotal from 'src/components/Table/CardTotal.vue';
 import { QTableColumn } from 'quasar';
 
 const cartItems = ref<OrderCart | null>(null);
-const customer = ref<Customer>({ id: 0, name: 'Guest', email: '', phone_no: '' });
+// const customer = ref<Customer>({ id: 0, name: 'Guest', email: '', phone_no: '' });
 
 const subTotal = computed(() => cartItems.value?.subTotal?.toFixed(2) ?? '0.00');
 const totalAmount = computed(() => cartItems.value?.totalAmount?.toFixed(2) ?? '0.00');
@@ -80,7 +80,7 @@ const discountAmount = computed(() => formatAmount(cartItems.value?.discount ?? 
 
 const updateCart = (data: OrderCart) => {
   cartItems.value = { ...data, surcharge_amount: data.surcharge_amount ?? 0, surcharge_type: data.surcharge_type ?? 0, discount: data.discount ?? 0 };
-  customer.value = data.customer || { id: 0, name: 'Guest', email: '', phone_no: '' };
+  // customer.value = data.customer || data.guestCustomerName || { id: 0, name: 'Guest', email: '', phone_no: '' };
 };
 
 const tableRows = computed(() => cartItems.value?.orderList?.map(item => ({
