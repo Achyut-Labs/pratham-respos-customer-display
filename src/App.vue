6844e1778cd968916dc6169f69d130450bdd0fac
achyutlabs-raghu <raghav.yadav@achyutlabs.com>
2024-10-04 12:30:12 +0530

    fix: fixed issue

--- src/App.vue
+++ src/App.vue
@@ -2,46 +2,58 @@
   <router-view />
 </template>
 
-<script lang="ts">
-import { defineComponent, onBeforeMount } from 'vue';
+<script lang="ts" setup>
 import { connectSocket } from './boot/local-socket';
 import { useRouter } from 'vue-router';
 import { useMediaSettingsStore } from './stores/media-settings-store';
+import { onMounted } from 'vue';
 
-const router = useRouter()
-const mediaSettingsStore = useMediaSettingsStore()
+const router = useRouter();
+const mediaSettingsStore = useMediaSettingsStore();
 
-export default defineComponent({
-  name: 'App'
-})
-const calculateAspectRatio = (screenSize: number) : number => {
-  let ar
+const calculateAspectRatio = (screenSize: number): number => {
+  let ar;
   if (screenSize === 50) {
-    ar = '8:9'
+    ar = '8:9';
   } else if (screenSize === 70) {
-    ar = '5:4'
+    ar = '5:4';
   } else {
-    ar = '16:9'
+    ar = '16:9';
   }
-  const availableAspectRatio = mediaSettingsStore.aspectRatios?.filter((item) => item.aspect_ratio === ar)
-  return availableAspectRatio ? availableAspectRatio[0].id : 0
-}
+  const availableAspectRatio = mediaSettingsStore.aspectRatios?.filter(
+    (item) => item.aspect_ratio === ar
+  );
+
+  return availableAspectRatio && availableAspectRatio?.length > 0
+    ? availableAspectRatio[0].id
+    : 0;
+};
 
 const getFiles = async () => {
-  mediaSettingsStore.screenSize = Number(localStorage.getItem('ScreenSize'))
-  mediaSettingsStore.group = Number(localStorage.getItem('group'))
-  mediaSettingsStore.restaurantId = Number(localStorage.getItem('RestaurantId'))
-  mediaSettingsStore.slideTransitionInterval = Number(localStorage.getItem('SlideTransitionInterval'))
-  const aspectRatioId = calculateAspectRatio(mediaSettingsStore.screenSize)
-  await mediaSettingsStore.getCustomerDisplayFile({restaurant_id: mediaSettingsStore.restaurantId, aspect_ratio_id: aspectRatioId, group_ids: String(mediaSettingsStore.group)})
-}
+  mediaSettingsStore.screenSize = Number(localStorage.getItem('ScreenSize'));
+  mediaSettingsStore.group = Number(localStorage.getItem('group'));
+  mediaSettingsStore.restaurantId = Number(
+    localStorage.getItem('RestaurantId')
+  );
+  mediaSettingsStore.slideTransitionInterval = Number(
+    localStorage.getItem('SlideTransitionInterval')
+  );
+  const aspectRatioId = calculateAspectRatio(mediaSettingsStore.screenSize);
+  await mediaSettingsStore.getCustomerDisplayFile({
+    restaurant_id: mediaSettingsStore.restaurantId,
+    aspect_ratio_id: aspectRatioId,
+    group_ids: String(mediaSettingsStore.group),
+  });
+};
 
-onBeforeMount(async () => {
-  const localSocketServerIp = localStorage.getItem('localSocketServerIp')
-  const localSocketServerPort = Number(localStorage.getItem('localSocketServerPort'))
-  if(localSocketServerIp && localSocketServerPort) {
-    connectSocket(localSocketServerIp, localSocketServerPort, router)
+onMounted(async () => {
+  const localSocketServerIp = localStorage.getItem('localSocketServerIp');
+  const localSocketServerPort = Number(
+    localStorage.getItem('localSocketServerPort')
+  );
+  if (localSocketServerIp && localSocketServerPort) {
+    connectSocket(localSocketServerIp, localSocketServerPort, router);
   }
-  getFiles()
-})
+  getFiles();
+});
 </script>
